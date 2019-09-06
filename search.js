4f3f705d012ca94ce57daa0144bc846681d72425
Ebrahim Alhaddad <ebrahimalhaddad@ebrahims-mbp-2.home>
2019-09-05 22:51:23 -0700

    final commit

--- search.js
+++ search.js
@@ -1,49 +1,32 @@
+
+const subredditTemplate = Handlebars.compile( document.getElementById('subreddit-template').innerHTML );
+
+Handlebars.registerHelper('number-format',function(number){
+	return number.toLocaleString();
+});
+
 $('#search_form').on('submit', function(){
 
 	$('#results').empty();
 	$('#spinner').css('display','block');
-
 	let endpoint = 'https://www.reddit.com/r/' + $('input').val() + '.json';
+
 	let promise = $.ajax({
 		type:'GET',
 		url:endpoint
 	});
 
 	promise.then(function(response){
-
-
+		console.log(response);
 		let response_array = response.data.children
-		let fragment = document.createDocumentFragment();
 
-		response_array.forEach(function(member){
-
-			let entry_div = document.createElement('div');
-			let title = document.createElement('a');
-			let score = document.createElement('h5');
-			let author = document.createElement('h5');
-			let seperator = document.createElement('hr');
-
-			title.innerText = member.data.title;
-			title.href = member.data.url;
-			title.target = "_blank";
-			score.innerText = `Score: ${member.data.score}`;
-			author.innerText = `Author: ${member.data.author}`;
-
-			entry_div.append(title);
-			entry_div.append(score);
-			entry_div.append(author);
-			entry_div.append(seperator);
-			fragment.append(entry_div);
-			
-
+		let sanitizedHtml = subredditTemplate({
+			subreddits:response_array
 		});
 
-		$('#results').html(fragment);
+		$('#results').html(sanitizedHtml);
 		$('#spinner').css('display','none');
-
-
 	});
-
 
 	return false;
 });
